9f6fdd01a62f58913d474e1df10b29adfa40c144
alasgar88 <mahmudov.alasgar@gmail.com>
2021-10-19 14:52:24 +0400

    quix fix get object by random id

--- trydjango/views.py
+++ trydjango/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse
 from articles.models import Article
 import random
 from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
 
 
 def home(request, *args, **kwargs):
     random_id = random.randint(1, 4)
-    article_obj = Article.objects.get(id=random_id)
-
+    article_obj = get_object_or_404(Article, id=random_id)
     object_list = Article.objects.all()
     context = {
         "object_list": object_list,
