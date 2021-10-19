ba64fcbe1d92b30df7e766c909c7e6c5fccbd4ba
alasgar88 <mahmudov.alasgar@gmail.com>
2021-10-19 19:45:37 +0400

    added publish field to Articles

--- articles/models.py
+++ articles/models.py
@@ -6,6 +6,8 @@ class Article(models.Model):
     content = models.TextField()
     timestamp = models.DateField(auto_now_add=True)
     update = models.DateField(auto_now=True)
+    publish = models.DateField(
+        auto_now=False, auto_now_add=False, null=True, blank=True,)
 
     def __str__(self):
         return self.title
